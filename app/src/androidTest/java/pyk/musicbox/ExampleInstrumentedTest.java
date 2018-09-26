4e833b61c2402f821f02ec140771ddd6370cc684
Peter Kennedy <pykennedy@gmail.com>
2018-09-25 18:39:44 -0700

    test complete

--- app/src/androidTest/java/pyk/musicbox/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/pyk/musicbox/ExampleInstrumentedTest.java
@@ -1,26 +1,27 @@
 package pyk.musicbox;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import pyk.musicbox.view.activity.MainActivity;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+  @Rule
+  public ActivityTestRule<MainActivity> mainActivityActivityTestRule =
+    new ActivityTestRule<MainActivity>(MainActivity.class);
+  
   @Test
-  public void useAppContext() {
-    // Context of the app under test.
-    Context appContext = InstrumentationRegistry.getTargetContext();
-
-    assertEquals("pyk.musicbox", appContext.getPackageName());
+  public void viewExists() throws Exception {
+    onView(withId(R.id.line1)).check(matches(isDisplayed()));
   }
 }
