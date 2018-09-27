2eb58bba3fd4aaf565ee2f8e89f74c414fd59fbe
Peter Kennedy <pykennedy@gmail.com>
2018-09-26 19:45:48 -0700

    set up base fragments

--- app/src/main/java/pyk/musicbox/view/activity/MainActivity.java
+++ app/src/main/java/pyk/musicbox/view/activity/MainActivity.java
@@ -1,27 +1,78 @@
 package pyk.musicbox.view.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 import pyk.musicbox.R;
 import pyk.musicbox.contract.MainActivityContract;
 import pyk.musicbox.presenter.MainActivityPresenter;
+import pyk.musicbox.view.fragment.HomeFragment;
 
 public class MainActivity extends AppCompatActivity
     implements MainActivityContract.MainActivityView {
   
   private MainActivityPresenter mainActivityPresenter;
+  private ViewPager pager;
+  private PagerAdapter pagerAdapter;
   
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     mainActivityPresenter = new MainActivityPresenter(this);
+    
+    pager = findViewById(R.id.vp_mainActivity);
+    pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+    pager.setAdapter(pagerAdapter);
+    pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+      @Override
+      public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+  
+      @Override
+      public void onPageSelected(int position) {}
+  
+      @Override
+      public void onPageScrollStateChanged(int state) {}
+    });
   }
   
   @Override
   public void showToast() {
     Toast.makeText(this, "it worked", Toast.LENGTH_SHORT).show();
   }
+
+  private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+  
+    ScreenSlidePagerAdapter(FragmentManager fm) {
+      super(fm);
+    }
+  
+    @Override
+    public Fragment getItem(int position) {
+      switch (position) {
+        case 0:
+          return new HomeFragment();
+//          return new TrackFragment();
+//          return new SettingsFragment();
+//          return new SearchFragment();
+//          return new SearchDetailsFragment();
+//          return new GroupsFragment();
+//          return new PlaylistsFragment();
+        default:
+          return new HomeFragment();
+      }
+    }
+  
+    @Override
+    public int getCount() {
+      return 1;
+    }
+  }
+  
 }
